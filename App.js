59bb4bb75f9b8238dfa489429ef23392bf2c1e1f
Brandon Hoff <brandonjhoff@me.com>
2021-08-11 11:00:20 -0600

    added tailwind and custom tailwind config

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import tw from './src/lib/tailwind';
 
-export default function App() {
+const desktop = 'flex-1 bg-pureblack-0 justify-center items-center'
+const mobile = 'flex-1 bg-primary-100 justify-center items-center'
+
+export default function App(props) {
+  const [layoutStyle, setLayoutStyle] = React.useState(desktop);
+
+  const handleLayout = ({ nativeEvent }) => {
+    if(nativeEvent.layout.width < 500) {
+      setLayoutStyle(mobile)
+    } else {
+      setLayoutStyle(desktop)
+    }
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View onLayout={handleLayout} style={tw`${layoutStyle}`}>
+      <Text style={tw`text-primary-900 text-16 font-700`}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
